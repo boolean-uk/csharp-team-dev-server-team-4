20f52a26469d10ff886378e5c2ee5c3e83dfd35d
Mathias Handeland <127216029+MathiasHandeland@users.noreply.github.com>
2025-08-27 15:29:26 +0200

    chore: uncommented code

--- exercise.wwwapi/Endpoints/UserEndpoints.cs
+++ exercise.wwwapi/Endpoints/UserEndpoints.cs
@@ -45,8 +45,8 @@ namespace exercise.wwwapi.EndPoints
         private static async Task<IResult> Register(RegisterRequestDTO request, IRepository<User> service, IValidator<RegisterRequestDTO> validator)
         {
             //user exists
-            //if (service.GetAll().Where(u => u.Email == request.email)
-              //  .Any()) return Results.Conflict(new ResponseDTO<RegisterFailureDTO>() { Status = "Fail" });
+            if (service.GetAll().Where(u => u.Email == request.email)
+                .Any()) return Results.Conflict(new ResponseDTO<RegisterFailureDTO>() { Status = "Fail" });
             
             // validate email
             var validation = await validator.ValidateAsync(request);
