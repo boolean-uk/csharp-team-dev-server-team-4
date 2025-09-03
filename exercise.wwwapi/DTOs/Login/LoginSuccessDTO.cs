cd4d3e8813e6cac19052c39e8c40069e4582a3c9
Jakub Mroz <kuba.mroz01@gmail.com>
2025-09-03 09:59:48 +0200

    fix: loginSuccessDto UserDto

--- exercise.wwwapi/DTOs/Login/LoginSuccessDTO.cs
+++ exercise.wwwapi/DTOs/Login/LoginSuccessDTO.cs
@@ -5,6 +5,6 @@ namespace exercise.wwwapi.DTOs.Login
     public class LoginSuccessDTO
     {
         public string token { get; set; }
-        public User user { get; set; } = new User();
+        public UserDTO user { get; set; } = new UserDTO();
     }
 }
