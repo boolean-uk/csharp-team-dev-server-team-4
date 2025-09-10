2e87465bd1f0bf6bbbd80cc60fc069967abc4923
Mathias Handeland <127216029+MathiasHandeland@users.noreply.github.com>
2025-09-10 15:10:02 +0200

    feat: get note by id tests

--- api.tests/Notes/GetNotesTests.cs
+++ api.tests/Notes/GetNotesTests.cs
@@ -2,6 +2,7 @@
 using exercise.wwwapi.DTOs.Login;
 using exercise.wwwapi.DTOs.Notes;
 using exercise.wwwapi.Endpoints;
+using exercise.wwwapi.Models;
 using exercise.wwwapi.Models.UserInfo;
 using System;
 using System.Collections.Generic;
@@ -27,6 +28,81 @@ namespace api.tests.Notes
         public void TearDown()
         {
             _client.Dispose();
+        }
+
+        [Test]
+        public async Task GetNoteByIdSuccess()
+        {
+            // login to a teacher user
+            var loginUser = new LoginRequestDTO
+            {
+                Email = "test2@test2",
+                Password = "Test2test2%"
+            };
+
+            var loginContent = new StringContent(
+                JsonSerializer.Serialize(loginUser),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            var loginResponse = await _client.PostAsync("login", loginContent);
+            Assert.That(loginResponse.IsSuccessStatusCode, Is.True);
+
+            var loginJson = await loginResponse.Content.ReadAsStringAsync();
+            var login = JsonSerializer.Deserialize<ResponseDTO<LoginSuccessDTO>>(loginJson);
+            Assert.That(login, Is.Not.Null);
+
+            _client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", login!.Data.Token);
+
+            var noteId = 1;
+            var response = await _client.GetAsync($"/notes/{noteId}");
+
+            Assert.That(response.IsSuccessStatusCode, Is.True);
+
+            var responseJson = await response.Content.ReadAsStringAsync();
+            var noteResult = JsonSerializer.Deserialize<ResponseDTO<NoteResponseDTO>>(responseJson,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            Assert.That(noteResult, Is.Not.Null);
+            Assert.That(noteResult.Status, Is.EqualTo("success"));
+            Assert.That(noteResult.Data, Is.Not.Null);
+            Assert.That(noteResult.Data.Id, Is.EqualTo(noteId));
+            Assert.That(noteResult.Data.Title, Is.EqualTo("Title Note 1"));
+            Assert.That(noteResult.Data.Content, Is.EqualTo("note1note1 note1 note1 content")); 
+        }
+
+        [Test]
+        public async Task GetNoteByIdFails()
+        {
+            // login to a teacher user
+            var loginUser = new LoginRequestDTO
+            {
+                Email = "test2@test2",
+                Password = "Test2test2%"
+            };
+
+            var loginContent = new StringContent(
+                JsonSerializer.Serialize(loginUser),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            var loginResponse = await _client.PostAsync("login", loginContent);
+            Assert.That(loginResponse.IsSuccessStatusCode, Is.True);
+
+            var loginJson = await loginResponse.Content.ReadAsStringAsync();
+            var login = JsonSerializer.Deserialize<ResponseDTO<LoginSuccessDTO>>(loginJson);
+            Assert.That(login, Is.Not.Null);
+
+            _client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", login!.Data.Token);
+
+            var noteId = 999; // note does not exist
+            var response = await _client.GetAsync($"/notes/{noteId}");
+
+            Assert.That(response.IsSuccessStatusCode, Is.False);
         }
 
         [Test]
@@ -67,7 +143,8 @@ namespace api.tests.Notes
             Assert.That(notesResult, Is.Not.Null);
             Assert.That(notesResult.Status, Is.EqualTo("success"));
             Assert.That(notesResult.Data, Is.Not.Null);
-            Assert.That(notesResult.Data.Notes, Is.Not.Empty); 
+            Assert.That(notesResult.Data.Notes, Is.Not.Empty);
+            Assert.That(notesResult.Data.Notes.Count, Is.EqualTo(1));
         }
 
         [Test]
@@ -109,6 +186,7 @@ namespace api.tests.Notes
             Assert.That(notesResult.Status, Is.EqualTo("success"));
             Assert.That(notesResult.Data, Is.Not.Null);
             Assert.That(notesResult.Data.Notes, Is.Empty);
+            Assert.That(notesResult.Data.Notes.Count, Is.EqualTo(0));
         }
     }
 }
