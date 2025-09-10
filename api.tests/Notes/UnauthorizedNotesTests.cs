4ad2abf70d15bf77f129f7a36d09ca2f79326439
Jakub Mroz <kuba.mroz01@gmail.com>
2025-09-10 13:53:33 +0200

    feat: initial notes tests

---
+++ api.tests/Notes/UnauthorizedNotesTests.cs
@@ -0,0 +1,169 @@
+﻿using exercise.wwwapi.DTOs;
+using exercise.wwwapi.DTOs.GetUsers;
+using exercise.wwwapi.DTOs.Login;
+using exercise.wwwapi.DTOs.Notes;
+using exercise.wwwapi.Endpoints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace api.tests.Notes;
+
+public class UnauthorizedNotesTests
+{
+    private HttpClient _client;
+    private StringContent? _contentLogin;
+    private StringContent? _contentCreate;
+
+    [SetUp]
+    public void Setup()
+    {
+        _client = TestUtils.CreateClient();
+
+        // login
+        var loginUser = new LoginRequestDTO
+        {
+            Email = "test1@test1",
+            Password = "Test1test1%"
+        };
+
+        var _contentLogin = new StringContent(
+            JsonSerializer.Serialize(loginUser),
+            Encoding.UTF8,
+            "application/json"
+        );
+
+        // POST note
+        var createNote = new CreateNoteRequestDTO
+        {
+            Title = "test",
+            Content = "test"
+        };
+        var _contentCreate = new StringContent(
+            JsonSerializer.Serialize(createNote),
+            Encoding.UTF8,
+            "application/json"
+        );
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _client.Dispose();
+
+        _contentLogin?.Dispose();        
+        _contentCreate?.Dispose();
+    }
+
+    [Test]
+    public async Task GetNoteByIdByNonUserReceiveUnauthorizedTest()
+    {
+        var response = await _client.GetAsync("/notes/1");
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Unauthorized));
+    }
+
+    [Test]
+    public async Task GetNoteByIdByStudentReceiveUnauthorizedTest()
+    {
+        var loginResponse = await _client.PostAsync("login", _contentLogin);
+        Assert.That(loginResponse.IsSuccessStatusCode, Is.True);
+
+        var jsonResponse = await loginResponse.Content.ReadAsStringAsync();
+        var result = JsonSerializer.Deserialize<ResponseDTO<LoginSuccessDTO>>(jsonResponse);
+        Assert.That(result, Is.Not.Null);
+
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result.Data.Token);
+
+        var response = await _client.GetAsync("/notes/1");
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Unauthorized));
+    }
+
+    [Test]
+    public async Task GetStudentNotesByNonUserReceiveUnauthorizedTest()
+    {
+        var response = await _client.GetAsync("/users/1/notes");
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Unauthorized));
+    }
+
+    [Test]
+    public async Task GetStudentNotesByStudentReceiveUnauthorizedTest()
+    {
+        var loginResponse = await _client.PostAsync("login", _contentLogin);
+        Assert.That(loginResponse.IsSuccessStatusCode, Is.True);
+
+        var jsonResponse = await loginResponse.Content.ReadAsStringAsync();
+        var result = JsonSerializer.Deserialize<ResponseDTO<LoginSuccessDTO>>(jsonResponse);
+        Assert.That(result, Is.Not.Null);
+
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result.Data.Token);
+
+        var response = await _client.GetAsync("/users/1/notes");
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Unauthorized));
+    }
+
+    [Test]
+    public async Task CreateStudentNoteByNonUserReceiveUnauthorizedTest()
+    {
+        var response = await _client.PostAsync("/users/1/notes", _contentCreate);
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Unauthorized));
+    }
+
+    [Test]
+    public async Task CreateStudentNoteByStudentReceiveUnauthorizedTest()
+    {
+        var loginResponse = await _client.PostAsync("login", _contentLogin);
+        Assert.That(loginResponse.IsSuccessStatusCode, Is.True);
+
+        var jsonResponse = await loginResponse.Content.ReadAsStringAsync();
+        var result = JsonSerializer.Deserialize<ResponseDTO<LoginSuccessDTO>>(jsonResponse);
+        Assert.That(result, Is.Not.Null);
+
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result.Data.Token);
+
+        var response = await _client.PostAsync("/users/1/notes", _contentCreate);
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Unauthorized));
+    }
+
+    [Test]
+    public async Task DeleteStudentNoteByNonUserReceiveUnauthorizedTest()
+    {
+        var response = await _client.DeleteAsync("/notes/1");
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Unauthorized));
+    }
+
+    [Test]
+    public async Task DeleteStudentNoteByStudentReceiveUnauthorizedTest()
+    {
+        var loginResponse = await _client.PostAsync("login", _contentLogin);
+        Assert.That(loginResponse.IsSuccessStatusCode, Is.True);
+
+        var jsonResponse = await loginResponse.Content.ReadAsStringAsync();
+        var result = JsonSerializer.Deserialize<ResponseDTO<LoginSuccessDTO>>(jsonResponse);
+        Assert.That(result, Is.Not.Null);
+
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result.Data.Token);
+
+        var response = await _client.GetAsync("/users/1/notes");
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Unauthorized));
+    }
+
+    [Test]
+    public async Task UpdateStudentNoteByNonUserReceiveUnauthorizedTest()
+    {
+        //TODO
+        var response = await _client.GetAsync("/users/1/notes");
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Unauthorized));
+    }
+
+    [Test]
+    public async Task UpdateStudentNoteByStudentReceiveUnauthorizedTest()
+    {
+        //TODO
+        var response = await _client.GetAsync("/users/1/notes");
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Unauthorized));
+    }
+}
