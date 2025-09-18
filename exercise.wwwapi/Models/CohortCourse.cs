115cd8f65ab3614c270224f3b9738f99574680cf
Johan Reitan <johan.s.reitan@gmail.com>
2025-09-18 13:32:17 +0200

    added added fils

---
+++ exercise.wwwapi/Models/CohortCourse.cs
@@ -0,0 +1,30 @@
+﻿using exercise.wwwapi.Repository;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace exercise.wwwapi.Models
+{
+    [Table("cohort_course")]
+    public class CohortCourse : IEntity
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Cohort))]
+        [Column("cohort_id")]
+        public int CohortId { get; set; }
+
+        [ForeignKey(nameof(Course))]
+        [Column("course_id")]
+        public int CourseId { get; set; }
+
+        public Cohort Cohort { get; set; }
+        public Course Course { get; set; }
+        public ICollection<UserCC> UserCCs { get; set; } = new List<UserCC>();
+
+
+
+    }
+}
