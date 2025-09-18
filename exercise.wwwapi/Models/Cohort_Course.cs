cbaf8c0f81d3e14c0aa90c245fe08fc12f57393e
Snorre Aldstedt <snorre.alstedt@experisacademy.no>
2025-09-18 09:14:47 +0200

    Add table usercc & cohort_course, update cohort and course table

---
+++ exercise.wwwapi/Models/Cohort_Course.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using exercise.wwwapi.Models.UserInfo;
+
+
+namespace exercise.wwwapi.Models
+{
+    public class CohortCourse
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Cohort))]
+        [Column("cohort_id")]
+        public int CohortId { get; set; }
+
+        [ForeignKey(nameof(Course))]
+        [Column("course_id")]
+        public int CourseId { get; set; }
+
+        public Cohort Cohort { get; set; }
+        public Course Course { get; set; }
+
+    }
+}
---
+++ exercise.wwwapi/Models/UserCC.cs
@@ -0,0 +1,30 @@
+﻿using exercise.wwwapi.Models.UserInfo;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace exercise.wwwapi.Models
+{
+    [Table("user_cc")]
+    public class UserCC
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(User))]
+        [Column("course_id")]
+        public int CourseId { get; set; }
+
+        [ForeignKey(nameof(User))]
+        [Column("user_id")]
+
+        public int UserId { get; set; }
+
+        public Course Course { get; set; }
+
+        public User User { get; set; }
+
+        public ICollection<User> Users { get; set; } = new List<User>();
+
+    }
+}
