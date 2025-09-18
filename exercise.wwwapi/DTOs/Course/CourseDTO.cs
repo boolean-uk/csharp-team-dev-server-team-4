b290b3f97ef94ddb73be467f582da308ff5780be
Trym Haugan Berger <111427044+598115@users.noreply.github.com>
2025-09-18 14:35:31 +0200

    Delete exercise.wwwapi/DTOs/Course/CourseDTO.cs

--- exercise.wwwapi/DTOs/Course/CourseDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace exercise.wwwapi.Models;
-public class CourseDTO
-{
-    public string? CourseName { get; set; }
-
-}
