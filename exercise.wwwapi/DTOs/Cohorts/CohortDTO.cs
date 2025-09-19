e9c4376a889cff3dee4b6a8acb731cd4636b57b9
Mona Eikli Andresen <monaeik@hotmail.com>
2025-09-19 12:13:53 +0200

    Created the cohort-endpoints

--- exercise.wwwapi/DTOs/Cohorts/CohortDTO.cs
+++ exercise.wwwapi/DTOs/Cohorts/CohortDTO.cs
@@ -2,17 +2,17 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
+using exercise.wwwapi.DTOs.Courses;
+using exercise.wwwapi.DTOs.Exercises;
 
 namespace exercise.wwwapi.Models;
 
 public class CohortDTO
 {
-    [JsonPropertyName("course_id")]
-
+    public int Id { get; set; }
     public int CohortNumber { get; set; }
-
     public string CohortName { get; set; }
-
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
+    public List<CourseDTO> Courses { get; set; }
 }
---
+++ exercise.wwwapi/DTOs/Courses/CourseDTO.cs
@@ -0,0 +1,16 @@
+﻿using exercise.wwwapi.Models;
+
+namespace exercise.wwwapi.DTOs.Courses
+{
+    public class CourseDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public CourseDTO() { }
+        public CourseDTO(Course model)
+        {
+            Id = model.Id;
+            Name = model.Name;
+        }
+    }
+}
