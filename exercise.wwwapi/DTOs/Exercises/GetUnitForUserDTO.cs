62f4a47711a3994522001ba5bb0eaa277aa844d4
Johan Reitan <johan.s.reitan@gmail.com>
2025-09-24 12:07:35 +0200

    created three dtos and changed exerciseEndpoint

---
+++ exercise.wwwapi/DTOs/Exercises/GetUnitForUserDTO.cs
@@ -0,0 +1,32 @@
+﻿using exercise.wwwapi.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace exercise.wwwapi.DTOs.Exercises
+{
+    public class GetUnitForUserDTO
+
+    {
+        public int Id { get; set; }
+        public int ModuleId { get; set; }
+        public string Name { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public ICollection<GetExerciseForUserDTO> Exercises { get; set; } = new List<GetExerciseForUserDTO>();
+
+        public GetUnitForUserDTO()
+        {
+            
+        }
+
+        public GetUnitForUserDTO(Unit model, ICollection<UserExercise> userExercises)
+        {
+            Id = model.Id;
+            ModuleId = model.ModuleId;
+            Name = model.Name;
+            Exercises = model.Exercises.Select(e => new GetExerciseForUserDTO(e, userExercises)).ToList();
+            IsCompleted = Exercises.All(e => e.isSubmitted);
+        }
+    }
+}
